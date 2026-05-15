9439b7257146c60dc7329b4eedf5ec66bad91054
tegardns <tegarputra723@gmail.com>
2026-05-15 18:02:01 +0700

    Add Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { AuthProvider } from "./context/AuthContext";
 import "./styles/index.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </AuthProvider>
   </React.StrictMode>,
 );
